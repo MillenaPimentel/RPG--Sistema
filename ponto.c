783f55db1effcf49ff163499bcf518e0ea60b629
Millena Costa Pimentel <millenacpimentel@gmail.com>
2025-11-28 13:50:15 -0300

    Add switch case structure for user selection

---
+++ ponto.c
@@ -0,0 +1,16 @@
+scanf("%selec", &selec);
+
+    switch (selec){
+        case 1:
+            // print da lista
+            break;
+        case 2:
+            // adicionar personagem
+        break;
+        case 3:
+            // excluir personagem
+        break;
+        case 4:
+            // iniciar combate void
+        break;
+    }
