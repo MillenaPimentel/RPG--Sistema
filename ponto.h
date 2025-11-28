1294f519ad226408812617fb7193f0eb9c9d9583
Millena Costa Pimentel <millenacpimentel@gmail.com>
2025-11-28 14:54:54 -0300

    Fix character addition logic in adicionarPersonagem

--- ponto.h
+++ ponto.h
@@ -15,6 +15,7 @@ int adicionarPersongem(){
     if(quant >= 50){
         printf(" voce ja atingiu o maximo de personagens\n ");
 }
+    else{
 Personagem p;
 printf("\n insira o nome: ");
 scanf("%d", &p.nome);
@@ -27,7 +28,10 @@ printf("\n personagem adicionado com sucesso!\n");
 printf(" nome: %c\n", p.nome);
 printf(" int: %d\n", p.nivel);
 printf(" dado: %d\n", p.dado);
+     quant++;
+    }
 }
 
 int removerPersonagem(){
+    
 }
