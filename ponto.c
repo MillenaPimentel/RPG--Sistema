c5425e040ee41a66359dbdd7c2e33863198d2350
Millena Costa Pimentel <millenacpimentel@gmail.com>
2025-12-02 10:56:45 -0300

    Remove Personagem struct definition
    Removed the Personagem struct definition from ponto.c.

--- ponto.c
+++ ponto.c
@@ -1,12 +1,5 @@
 #include "ponto.h"
 
-typedef struct{
-    char nome[50];
-    int nivel;
-    int dado;
-    int iniciativa;
-}Personagem;
-
 int rolagem(int dado){
     return rand() % (dado + 1);
 }
