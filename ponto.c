c0ae699a6261741cc9746029c4320e1b24386b56
Millena Costa Pimentel <millenacpimentel@gmail.com>
2025-12-02 12:34:31 -0300

    Update ponto.c

--- ponto.c
+++ ponto.c
@@ -4,25 +4,27 @@
 int quantPersonagens = 0;
 personagem lista[50];
 
-int adicionarPersongem(){
+int adicionarPersongem(void){
     if(quantPersonagens >= 50){
         printf(" voce ja atingiu o maximo de personagens\n ");
 }
     else{
-Personagem p;
-printf("\n insira o nome: ");
-scanf("%d", &p.nome);
-printf("\n insira o nivel: ");
-scanf("%d", &p.nivel);
-printf("\n insira o dado: ");
-scanf("%d", &p.dado);
-
-printf("\n personagem adicionado com sucesso!\n");
-printf(" nome: %c\n", p.nome);
-printf(" int: %d\n", p.nivel);
-printf(" dado: %d\n", p.dado);
+    Personagem *p = &lista[quantPersonagens];
+    printf("\n insira o nome: ");
+    scanf("%c", &p.nome);
+    printf("\n insira o nivel: ");
+    scanf("%d", &p.nivel);
+    printf("\n insira o dado: ");
+    scanf("%d", &p.dado);
+    p->iniciativa = rolagem(p->dado);
+    printf("\n personagem adicionado com sucesso!\n");
+    printf("esse é o ID do seu personagem: %d \n", quantPersonagens);
+    printf(" nome: %c\n", p->nome);
+    printf(" int: %d\n", p->nivel);
+    printf(" dado: %d\n", p->dado);
      quantPersonagens++;
     }
+    
 }
 
 int rolagem(int dado){
