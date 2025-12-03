d8a87a3d5a87565a0f946f39c493754b272810fc
Rodrigo Frota <156903745+RodrigoFrota@users.noreply.github.com>
2025-12-02 22:48:27 -0300

    Update and rename ponto.h to rpg.h

---
+++ rpg.h
@@ -0,0 +1,26 @@
+#ifndef RPG_H
+#define RPG_H 
+#define MAX_PERSONAGENS 50
+
+typedef struct{
+    char nome[16];
+    int nivel;  // 1 a 20
+    int dado;  // d4, d6, d8, d10, d12 ou d20 
+    int iniciativa;  // nível + rolagem
+} Personagem;
+
+int escolherDado();
+int rolarDado(int dado);
+void criarPersonagem(Personagem *lista, int *qtd);
+void editarPersonagem(Personagem *lista, int *qtd);
+void removerPersonagem(Personagem *lista, int *qtd);
+void listarPersonagens(Personagem *lista, int qtd);
+void calcularIniciativa(Personagem *lista, int qtd);
+void ordenar(Personagem *lista, int qtd, int metodoOrdenacao);
+
+void selecionarMetodoOrdenacao(int *metodoOrdenacao);
+void exibirOrdemDeAcao(Personagem lista[], int qtd);
+void loopCombate(Personagem lista[], int *qtd, int *metodoOrdenacao);
+int  menuPrincipal();
+
+#endif
