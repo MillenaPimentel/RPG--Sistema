efa7575812d48ab0544fe248b21c56f726e30b53
LeticiaBTS <barbosa.leticia753@gmail.com>
2025-12-04 12:33:46 -0300

    adiciona comentários rpg.h

--- rpg.h
+++ rpg.h
@@ -9,21 +9,20 @@ typedef struct{
     int iniciativa;  // nível + rolagem
 } Personagem;
 
-int escolherDado();
-int rolarDado(int dado);
-void adicionarArquivo(int *qtd, Personagem *lista);
-void salvarPersonagens(int qtd, Personagem *lista);
-void carregarArquivo(int *qtd, Personagem *lista);
-void criarPersonagem(Personagem *lista, int *qtd);
-void editarPersonagem(Personagem *lista, int *qtd);
-void removerPersonagem(Personagem *lista, int *qtd);
-void listarPersonagens(Personagem *lista, int qtd);
-void calcularIniciativa(Personagem *lista, int qtd);
-void ordenar(Personagem *lista, int qtd, int metodoOrdenacao);
-
-void selecionarMetodoOrdenacao(int *metodoOrdenacao);
-void exibirOrdemDeAcao(Personagem lista[], int qtd);
-void loopCombate(Personagem lista[], int *qtd, int *metodoOrdenacao);
-int  menuPrincipal();
+int escolherDado(); //escolhe o tipo de dado do menu(0 a 20)
+int rolarDado(int dado); //rola dado aleatório (1 a N faces)
+void adicionarArquivo(int *qtd, Personagem *lista); //importa personagem de arquivo externo
+void salvarPersonagens(int qtd, Personagem *lista); //salva lista em "personagens.txt"
+void carregarArquivo(int *qtd, Personagem *lista); // Carrega lista do "personagens.txt" no início
+void criarPersonagem(Personagem *lista, int *qtd); // Cria novo personagem via input
+void editarPersonagem(Personagem *lista, int *qtd); // Edita personagem por índice
+void removerPersonagem(Personagem *lista, int *qtd); // Remove personagem por índice
+void listarPersonagens(Personagem *lista, int qtd); // Lista todos os personagens
+void calcularIniciativa(Personagem *lista, int qtd); // Calcula iniciativa para toda a lista
+void ordenar(Personagem *lista, int qtd, int metodoOrdenacao); // Ordena por iniciativa (Quick/Insertion Sort)
+void selecionarMetodoOrdenacao(int *metodoOrdenacao); // Menu de escolha QuickSort/InsertionSort
+void exibirOrdemDeAcao(Personagem lista[], int qtd); // Mostra ordem de ação do combate
+void loopCombate(Personagem lista[], int *qtd, int *metodoOrdenacao); // Loop principal do combate RPG
+int  menuPrincipal(); // Menu principal (todas as opções)
 
 #endif
